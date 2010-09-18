59b489d88a9a2352eb043a4c939c5f687dd9e0f4
Adrian Sampson <adrian@radbox.org>
2010-09-18 10:51:16 -0700

    fix autotagging of artist !!! via special-casing it

---
+++ test/test_files.py
@@ -0,0 +1,199 @@
+# This file is part of beets.
+# Copyright 2010, Adrian Sampson.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+"""Test file manipulation functionality of Item.
+"""
+
+import unittest
+import shutil
+import sys
+import os
+from os.path import join
+sys.path.append('..')
+import beets.library
+from test_db import item
+
+def touch(path):
+    open(path, 'a').close()
+
+class MoveTest(unittest.TestCase):
+    def setUp(self):
+        # make a temporary file
+        self.path = join('rsrc', 'temp.mp3')
+        shutil.copy(join('rsrc', 'full.mp3'), self.path)
+        
+        # add it to a temporary library
+        self.lib = beets.library.Library(':memory:')
+        self.i = beets.library.Item.from_path(self.path)
+        self.lib.add(self.i)
+        
+        # set up the destination
+        self.libdir = join('rsrc', 'testlibdir')
+        self.lib.directory = self.libdir
+        self.lib.path_format = join('$artist', '$album', '$title')
+        self.i.artist = 'one'
+        self.i.album = 'two'
+        self.i.title = 'three'
+        self.dest = join(self.libdir, 'one', 'two', 'three.mp3')
+        
+    def tearDown(self):
+        if os.path.exists(self.path):
+            os.remove(self.path)
+        if os.path.exists(self.libdir):
+            shutil.rmtree(self.libdir)
+    
+    def test_move_arrives(self):
+        self.i.move(self.lib)
+        self.assertTrue(os.path.exists(self.dest))
+    
+    def test_move_departs(self):
+        self.i.move(self.lib)
+        self.assertTrue(not os.path.exists(self.path))
+    
+    def test_copy_arrives(self):
+        self.i.move(self.lib, copy=True)
+        self.assertTrue(os.path.exists(self.dest))
+    
+    def test_copy_does_not_depart(self):
+        self.i.move(self.lib, copy=True)
+        self.assertTrue(os.path.exists(self.path))
+    
+    def test_move_changes_path(self):
+        self.i.move(self.lib)
+        self.assertEqual(self.i.path, beets.library._normpath(self.dest))
+    
+class HelperTest(unittest.TestCase):
+    def test_ancestry_works_on_file(self):
+        p = '/a/b/c'
+        a =  ['/','/a','/a/b']
+        self.assertEqual(beets.library._ancestry(p), a)
+    def test_ancestry_works_on_dir(self):
+        p = '/a/b/c/'
+        a = ['/', '/a', '/a/b', '/a/b/c']
+        self.assertEqual(beets.library._ancestry(p), a)
+    def test_ancestry_works_on_relative(self):
+        p = 'a/b/c'
+        a = ['a', 'a/b']
+        self.assertEqual(beets.library._ancestry(p), a)
+    
+    def test_components_works_on_file(self):
+        p = '/a/b/c'
+        a =  ['/', 'a', 'b', 'c']
+        self.assertEqual(beets.library._components(p), a)
+    def test_components_works_on_dir(self):
+        p = '/a/b/c/'
+        a =  ['/', 'a', 'b', 'c']
+        self.assertEqual(beets.library._components(p), a)
+    def test_components_works_on_relative(self):
+        p = 'a/b/c'
+        a =  ['a', 'b', 'c']
+        self.assertEqual(beets.library._components(p), a)
+
+class AlbumFileTest(unittest.TestCase):
+    def setUp(self):
+        # Make library and item.
+        self.lib = beets.library.Library(':memory:')
+        self.libdir = os.path.join('rsrc', 'testlibdir')
+        self.lib.directory = self.libdir
+        self.i = item()
+        # Make a file for the item.
+        self.i.path = self.lib.destination(self.i)
+        beets.library._mkdirall(self.i.path)
+        touch(self.i.path)
+        # Make an album.
+        self.ai = self.lib.add_album((self.i,))
+    def tearDown(self):
+        if os.path.exists(self.libdir):
+            shutil.rmtree(self.libdir)
+
+    def test_albuminfo_move_changes_paths(self):
+        self.ai.album = 'newAlbumName'
+        self.ai.move()
+        self.lib.load(self.i)
+
+        self.assert_('newAlbumName' in self.i.path)
+
+    def test_albuminfo_move_moves_file(self):
+        oldpath = self.i.path
+        self.ai.album = 'newAlbumName'
+        self.ai.move()
+        self.lib.load(self.i)
+
+        self.assertFalse(os.path.exists(oldpath))
+        self.assertTrue(os.path.exists(self.i.path))
+
+    def test_albuminfo_move_copies_file(self):
+        oldpath = self.i.path
+        self.ai.album = 'newAlbumName'
+        self.ai.move(True)
+        self.lib.load(self.i)
+
+        self.assertTrue(os.path.exists(oldpath))
+        self.assertTrue(os.path.exists(self.i.path))
+
+class ArtFileTest(unittest.TestCase):
+    def setUp(self):
+        # Make library and item.
+        self.lib = beets.library.Library(':memory:')
+        self.libdir = os.path.join('rsrc', 'testlibdir')
+        self.lib.directory = self.libdir
+        self.i = item()
+        self.i.path = self.lib.destination(self.i)
+        # Make a music file.
+        beets.library._mkdirall(self.i.path)
+        touch(self.i.path)
+        # Make an album.
+        self.ai = self.lib.add_album((self.i,))
+        # Make an art file too.
+        self.art = self.lib.get_album(self.i).art_destination('something.jpg')
+        touch(self.art)
+        self.ai.artpath = self.art
+    def tearDown(self):
+        if os.path.exists(self.libdir):
+            shutil.rmtree(self.libdir)
+
+    def test_art_deleted_when_items_deleted(self):
+        self.assertTrue(os.path.exists(self.art))
+        self.ai.remove(True)
+        self.assertFalse(os.path.exists(self.art))
+
+    def test_art_moves_with_album(self):
+        self.assertTrue(os.path.exists(self.art))
+        oldpath = self.i.path
+        self.ai.artist = 'newArtist'
+        self.ai.move()
+        self.lib.load(self.i)
+
+        self.assertNotEqual(self.i.path, oldpath)
+        self.assertFalse(os.path.exists(self.art))
+        newart = self.lib.get_album(self.i).art_destination(self.art)
+        self.assertTrue(os.path.exists(newart))
+
+    def test_setart_copies_image(self):
+        newart = os.path.join(self.libdir, 'newart.jpg')
+        touch(newart)
+        i2 = item()
+        i2.path = self.i.path
+        i2.artist = 'someArtist'
+        ai = self.lib.add_album((i2,))
+        i2.move(self.lib, True)
+        self.assertEqual(ai.artpath, None)
+        ai.set_art(newart)
+        self.assertTrue(os.path.exists(ai.artpath))
+
+def suite():
+    return unittest.TestLoader().loadTestsFromName(__name__)
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
---
+++ test/test_ui.py
@@ -0,0 +1,90 @@
+# This file is part of beets.
+# Copyright 2010, Adrian Sampson.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+"""Tests for the command-line interface.
+"""
+
+import unittest
+import sys
+import os
+sys.path.append('..')
+from beets import library
+from beets import ui
+from beets.ui import commands
+import test_db
+
+# Dummy printing so we can get the commands' output.
+outbuffer = []
+def buffer_append(*txt):
+    outbuffer.extend(txt)
+def get_output():
+    return u' '.join(outbuffer)
+def clear_buffer():
+    outbuffer[:]
+
+class ListTest(unittest.TestCase):
+    def setUp(self):
+        self.old_print = ui.print_
+        ui.print_ = buffer_append
+        commands.print_ = buffer_append
+
+        clear_buffer()
+        self.lib = library.Library(':memory:')
+        i = test_db.item()
+        self.lib.add(i)
+
+    def tearDown(self):
+        ui.print_ = self.old_print
+        commands.print_ = self.old_print
+        
+    def test_list_outputs_item(self):
+        commands.list_items(self.lib, '', False)
+        out = get_output()
+        self.assertTrue(u'the title' in out)
+    
+    def test_list_album_omits_title(self):
+        commands.list_items(self.lib, '', True)
+        out = get_output()
+        self.assertTrue(u'the title' not in out)
+    
+class PrintTest(unittest.TestCase):
+    def setUp(self):
+        class Devnull(object):
+            def write(self, d):
+                pass
+        self.stdout = sys.stdout
+        sys.stdout = Devnull()
+
+    def tearDown(self):
+        sys.stdout = self.stdout
+    
+    def test_print_without_locale(self):
+        lang = os.environ.get('LANG')
+        if lang:
+            del os.environ['LANG']
+
+        try:
+            ui.print_(u'something')
+        except TypeError:
+            self.fail('TypeError during print')
+
+        if lang:
+            os.environ['LANG'] = lang
+
+def suite():
+    return unittest.TestLoader().loadTestsFromName(__name__)
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
+
